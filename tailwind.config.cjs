d9a4c36d5dc25d8f131e004878af686322f06ffb
Mohammad <mohammad99yousefi@gmail.com>
2025-11-03 00:02:28 +0330

    Added a Custom Shadow Box

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+      'custom': '0 2px 1px rgba(0, 0, 0, 0.1)',
+      'custom-hover': '0 4px 4px rgba(0, 0, 0, 1.0)',
+      },
+    },
   },
   plugins: [],
 }
